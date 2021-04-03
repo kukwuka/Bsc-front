213cca7df7d887f34928b3aa8be7279f38222445
Valery <Rejke@yandex.ru>
2021-04-03 20:39:34 +0300

    Added SQLite database to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { InvestorsModule } from './investors/investors.module';
 
@@ -7,6 +8,12 @@ import { InvestorsModule } from './investors/investors.module';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/dist'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
+      synchronize: true,
     }),
     InvestorsModule,
   ],
--- src/investors/investors.module.ts
+++ src/investors/investors.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Investor } from './entities/investor.entity';
 import { InvestorsController } from './investors.controller';
 import { InvestorsService } from './investors.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Investor])],
   controllers: [InvestorsController],
-  providers: [InvestorsService]
+  providers: [InvestorsService],
 })
 export class InvestorsModule {}
