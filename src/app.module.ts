b251007c78db9932b618b7254d8aa967d2fbeeb0
Valery <Rejke@yandex.ru>
2021-04-03 20:03:31 +0300

    Cleaned app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -10,7 +8,5 @@ import { AppService } from './app.service';
       rootPath: join(__dirname, '..', 'client/dist'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
