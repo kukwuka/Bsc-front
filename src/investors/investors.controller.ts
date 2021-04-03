d75f2fb0ede4dd931a3f27158dde176dcb36b1e2
Valery <Rejke@yandex.ru>
2021-04-03 21:04:43 +0300

    Updated docs for Swagger

--- src/investors/investors.controller.ts
+++ src/investors/investors.controller.ts
@@ -1,8 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { Crud } from '@nestjsx/crud';
 import { Investor } from './entities/investor.entity';
 import { InvestorsService } from './investors.service';
 
+@ApiTags('investors')
 @Crud({
   model: {
     type: Investor,
