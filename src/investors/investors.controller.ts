2b29c20da2ae77046cadbbf79b6e3f06d7a3be61
Valery <Rejke@yandex.ru>
2021-04-03 20:30:33 +0300

    Updated investors controller and service for CRUD

--- src/investors/investors.controller.ts
+++ src/investors/investors.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { Crud } from '@nestjsx/crud';
+import { Investor } from './entities/investor.entity';
+import { InvestorsService } from './investors.service';
 
+@Crud({
+  model: {
+    type: Investor,
+  },
+})
 @Controller('investors')
-export class InvestorsController {}
+export class InvestorsController {
+  constructor(public service: InvestorsService) {}
+}
--- src/investors/investors.service.ts
+++ src/investors/investors.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { Investor } from './entities/investor.entity';
 
 @Injectable()
-export class InvestorsService {}
+export class InvestorsService extends TypeOrmCrudService<Investor> {
+  constructor(@InjectRepository(Investor) repo) {
+    super(repo);
+  }
+}
