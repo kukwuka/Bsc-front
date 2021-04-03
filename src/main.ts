6e4d3d3299756c93785d594227179c947b9313f9
Valery <Rejke@yandex.ru>
2021-04-03 20:48:39 +0300

    Implemented Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,16 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Invest notify')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('investors')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
